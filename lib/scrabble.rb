44384e42eef8a30152bf4cf0b637cebd85eccbd4
Asia Kane and Grady Shelton <asia@>
2017-04-18 09:21:27 -0700

    refactor if elses into one if by creating all values array to store all value keys and loop through them with .each_with_index within letters.each loop

--- lib/scrabble.rb
+++ lib/scrabble.rb
@@ -2,16 +2,17 @@ class String
   define_method(:scrabble) do
     value_one = ["a", "e", "i", "o", "u", "l", "n", "r", "s", "t"]
     value_two = ["d", "g"]
+    all_values = [value_one, value_two]
     scrabble_scores = Hash.new()
     scrabble_scores = {value_one => 1, value_two => 2}
     letters = self.downcase().split("")
 
     total_score = 0
     letters.each() do |letter|
-      if value_one.include?(letter)
-        total_score += scrabble_scores.fetch(value_one)
-      elsif value_two.include?(letter)
-        total_score += scrabble_scores.fetch(value_two)
+      all_values.each_with_index() do |array,index|
+        if array.include?(letter)
+          total_score += scrabble_scores.fetch(all_values[index])
+        end
       end
     end
     total_score
