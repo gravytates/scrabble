e645abb8fe57e91c7682b8e33be514490fc8b2bf
Asia Kane and Grady Shelton <grady@>
2017-04-18 09:10:02 -0700

    add second test to take multiple letters from the value_one array and add their point total together

--- lib/scrabble.rb
+++ lib/scrabble.rb
@@ -1,9 +1,9 @@
 class String
   define_method(:scrabble) do
-    value_one = ["a"]
+    value_one = ["a", "e", "i", "o", "u", "l", "n", "r", "s", "t"]
     scrabble_scores = Hash.new()
     scrabble_scores = {value_one => 1}
-    letters = self.split("")
+    letters = self.downcase().split("")
 
     total_score = 0
     letters.each() do |letter|
--- spec/scrabble_spec.rb
+++ spec/scrabble_spec.rb
@@ -5,4 +5,8 @@ describe('String#scrabble') do
   it('take "a" and return a scrabble score of 1') do
     expect('a'.scrabble()).to(eq(1))
   end
+
+  it('take "au" and return a scrabble score of 2') do
+    expect('AU'.scrabble()).to(eq(2))
+  end
 end
