1c92de7d29086cac292dc37211cafc5d6b286fbe
Asia Kane and Grady Shelton <grady@>
2017-04-18 16:14:47 -0700

    install capybara gem to do integration testing. have test fail, then provide correct input to have test pass

---
+++ spec/scrabble_integration_spec.rb
@@ -0,0 +1,14 @@
+require('capybara/rspec')
+require('./app')
+Capybara.app = Sinatra::Application
+set(:show_exceptions, false)
+
+describe('the scrabble path', {:type => :feature}) do
+  it('processes user entry and returns scrabble score') do
+    visit('/')
+    fill_in('word', :with => 'nice')
+    click_button('Show my score!')
+    expect(page).to have_content('6')
+
+  end
+end
